58c3f2dc2071e176a497a283e87d8249f18c797a
ben <aj981000@yahoo.com>
2018-09-28 12:34:04 -1000

    test

--- src/com/srcodes/example/alohaworld.java
+++ src/com/srcodes/example/alohaworld.java
@@ -40,6 +40,8 @@ public class alohaworld extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
+		doGet(request, response);
+		doGet(request, response);
 	}
 
 }
